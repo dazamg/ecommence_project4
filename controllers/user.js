b4c9b70d0fe7412f33501db8fb6714a4029305bb
dazmag <iscope09@hotmail.com>
2021-03-24 23:11:58 -0400

    cart model,user route and controller

---
+++ controllers/user.js
@@ -0,0 +1,60 @@
+const User = require('../models/user')
+const Product = require('../models/product')
+const Cart = require('../models/cart')
+
+exports.userCart = async(req, res) => {
+    // console.log(req.body)
+    const { cart } = req.body;
+
+    let products = []
+
+    const user = await User.findOne({email: req.user.email}).exec()
+
+    //check if cart with logged in user id already exist
+    let cartExistByThisUser = await Cart.findOne({ orderedBy: user._id}).exec();
+
+    if(cartExistByThisUser) {
+        cartExistByThisUser.remove()
+        console.log("removed old cart");
+    }
+
+    for(let i = 0; i < cart.length; i++) {
+        let cartObj = {};
+
+        cartObj.product = cart[i]._id
+        cartObj.count = cart[i].count
+        cartObj.color = cart[i].color
+        // get price for sub Total
+        let {price} = await Product.findById(cart[i]._id).select("price").exec()
+        cartObj.price = price;
+
+        products.push(cartObj)
+    }
+    // console.log('products', products)
+
+    let cartTotal = 0
+    for(let i = 0; i < products.length; i++) {
+        cartTotal = cartTotal + products[i].price * products[i].count
+    }
+    // console.log("cartTotal", cartTotal) 
+
+    let newCart = await new Cart({
+        products,
+        cartTotal,
+        orderedBy: user._id,
+    }).save();
+    console.log("<----new cart@@@@@@@", newCart);
+    res.json({ ok: true})
+}
+
+exports.getUserCart = async(req, res) => {
+    const user = await User.findOne({ email: req.user.email }).exec();
+
+    let cart = await Cart.findOne({ orderedBy: user._id }).populate(
+        "products.product",
+        "_id title price totalAfterDiscount")
+        .exec();
+
+        const {products, cartTotal, totalAfterDiscount} = cart
+        res.json({products, cartTotal, totalAfterDiscount}); // req.data. to obtain object in the cart in the frontend
+}
--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,21 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/user", (req, res) => {
-  res.json({
-    data: "hey you hit user API endpoint",
-  });
-});
+// middlewares
+const { authCheck, adminCheck } = require("../middlewares/auth");
+
+//controllers
+const {userCart, getUserCart} = require("../controllers/user")
+
+//save cart
+router.post('/user/cart', authCheck, userCart)
+// get cart
+router.get('/user/cart', authCheck, getUserCart)
+
+// router.get("/user", (req, res) => {
+//   res.json({
+//     data: "hey you hit user API endpoint",
+//   });
+// });
 
 module.exports = router;
